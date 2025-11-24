f7253b85a601d0d905130f08a8f351223c491513
toferx <mrsebipro@gmail.com>
2025-11-24 19:44:52 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'FX Studios'
+description 'Anti-Wanted System - Removes wanted levels automatically'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
